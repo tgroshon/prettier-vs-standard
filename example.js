61ee82d4f8c124e3b41149170a84f5e80d44da22
Thomas Groshong <thomas.groshong@zendesk.com>
2020-12-07 12:20:00 -0500

    export const f

--- example.js
+++ example.js
@@ -1,2 +1,2 @@
-const f = function(a) { if (a === 0) { return []; } return [1,2,3,4,a].map(int => int * 2); }
+export const f = function(a) { if (a === 0) { return []; } return [1,2,3,4,a].map(int => int * 2); }
 
